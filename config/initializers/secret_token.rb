7762a71527639f8c0caed7ab21c26dc1fa37ad1b
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2011-05-28 11:56:25 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicUploader::Application.config.secret_token = 'ebfbad76703323ffeb762358b659421ba08b264d1afde70bb23daf1ac8ec8138d187125128444bbef633a7e839890003c82bd9dacb52400f74d9b04a374f24b9'
